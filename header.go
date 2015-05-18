64093c85a0e5f96438d364ce5b1cce16c3c2634c
Solovev <mrbroume@gmail.com>
2015-05-18 14:00:12 +0400

    Header section

--- header.go
+++ header.go
@@ -32,9 +32,59 @@ func newHeader() (*HeaderSection, error) {
 		return nil, err
 	}
 	if ver != 1 {
-		return nil, errors.New(fmt.Sprintf("Wrong document version: %s!", ver))
+		return nil, errors.New(fmt.Sprintf("Wrong document version: %d!", ver))
 	}
 	header.Version = ver
+
+	err = reader.Skip(6)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := reader.ReadInt16()
+	if err != nil {
+		return nil, err
+	}
+	if ch < 1 || ch > 56 {
+		return nil, errors.New(fmt.Sprintf("Wrong number of channels: %d! Supported range is 1 to 56.", ch))
+	}
+	header.Channels = ch
+
+	h, err := reader.ReadInt32()
+	if err != nil {
+		return nil, err
+	}
+	if h < 1 || h > 30000 {
+		return nil, errors.New(fmt.Sprintf("Wrong document height: %d! Supported range is 1 to 30000.", h))
+	}
+	header.Height = h
+
+	w, err := reader.ReadInt32()
+	if err != nil {
+		return nil, err
+	}
+	if w < 1 || w > 30000 {
+		return nil, errors.New(fmt.Sprintf("Wrong document width: %d! Supported range is 1 to 30000.", w))
+	}
+	header.Width = w
+
+	d, err := reader.ReadInt16()
+	if err != nil {
+		return nil, err
+	}
+	if d != 1 && d != 8 && d != 16 && d != 32 {
+		return nil, errors.New(fmt.Sprintf("Wrong document depth: %d! Supported values are 1, 8, 16 and 32.", d))
+	}
+	header.Depth = d
+
+	c, err := reader.ReadInt16()
+	if err != nil {
+		return nil, err
+	}
+	if c < 0 || c > 9 {
+		return nil, errors.New(fmt.Sprintf("Wrong document depth: %c! Supported values are: Bitmap = 0; Grayscale = 1; Indexed = 2; RGB = 3; CMYK = 4; Multichannel = 7; Duotone = 8; Lab = 9.", d))
+	}
+	header.ColorMode = c
 
 	return header, nil
 }
--- reader.go
+++ reader.go
@@ -28,3 +28,18 @@ func (r *Reader) ReadInt16() (int16, error) {
 	}
 	return value, nil
 }
+
+func (r *Reader) ReadInt32() (int32, error) {
+	var value int32
+	if err := binary.Read(r.buf, binary.BigEndian, &value); err != nil {
+		return 0, err
+	}
+	return value, nil
+}
+
+func (r *Reader) Skip(n int) error {
+	if _, err := r.buf.Seek(int64(n), 1); err != nil {
+		return err
+	}
+	return nil
+}
