1fdeadc0f4e3f5de01f19161ee8be66946b987fe
Solovev <mrbroume@gmail.com>
2015-05-21 19:48:28 +0400

    Color mode

--- section_color_mode.go
+++ section_color_mode.go
@@ -1,1 +1,11 @@
 package gopsd
+
+func readColorMode(doc *Document) {
+	length := reader.ReadInt32()
+	if doc.ColorMode == "Indexed" {
+		// TODO
+	} else if doc.ColorMode == "Duotone" {
+		// TODO
+	}
+	reader.Skip(length)
+}
