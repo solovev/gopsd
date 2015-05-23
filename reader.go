785a60f9ed5fb8c097e7971a183d91a1004223ee
Solovev <mrbroume@gmail.com>
2015-05-23 08:38:56 +0400

    Impl. resources + thumb

--- reader.go
+++ reader.go
@@ -6,62 +6,69 @@ import (
 )
 
 type Reader struct {
-	buf *bytes.Reader
+	buf      *bytes.Reader
+	Position int
 }
 
 func NewReader(b []byte) *Reader {
-	return &Reader{bytes.NewReader(b)}
+	return &Reader{bytes.NewReader(b), 0}
 }
 
-func (r Reader) ReadByte() byte {
+func (r *Reader) ReadByte() byte {
 	var value byte
 	if err := binary.Read(r.buf, binary.BigEndian, &value); err != nil {
 		panic(err)
 	}
+	r.Position++
 	return value
 }
 
-func (r Reader) ReadString32() string {
+func (r *Reader) ReadString32() string {
 	value := make([]byte, 4)
 	if err := binary.Read(r.buf, binary.BigEndian, &value); err != nil {
 		panic(err)
 	}
+	r.Position += 4
 	return string(value)
 }
 
-func (r Reader) ReadInt16() int16 {
+func (r *Reader) ReadInt16() int16 {
 	var value int16
 	if err := binary.Read(r.buf, binary.BigEndian, &value); err != nil {
 		panic(err)
 	}
+	r.Position += 2
 	return value
 }
 
-func (r Reader) ReadInt32() int32 {
+func (r *Reader) ReadInt32() int32 {
 	var value int32
 	if err := binary.Read(r.buf, binary.BigEndian, &value); err != nil {
 		panic(err)
 	}
+	r.Position += 4
 	return value
 }
 
-func (r Reader) ReadFloat32() float32 {
+func (r *Reader) ReadFloat32() float32 {
 	var value float32
 	if err := binary.Read(r.buf, binary.BigEndian, &value); err != nil {
 		panic(err)
 	}
+	r.Position += 4
 	return value
 }
 
-func (r Reader) ReadFloat64() float64 {
+func (r *Reader) ReadFloat64() float64 {
 	var value float64
 	if err := binary.Read(r.buf, binary.BigEndian, &value); err != nil {
 		panic(err)
 	}
+	r.Position += 8
 	return value
 }
 
-func (r Reader) ReadPascalString() string {
+func (r *Reader) ReadPascalString() string {
 	var l byte
 	if err := binary.Read(r.buf, binary.BigEndian, &l); err != nil {
 		panic(err)
@@ -69,10 +76,11 @@ func (r Reader) ReadPascalString() string {
 	if l%2 == 0 {
 		l = 1
 	}
+	r.Position += int(l) + 1
 
 	if l == 1 {
 		reader.Skip(1)
-		return ""
+		return "?"
 	} else {
 		value := make([]byte, l)
 		if err := binary.Read(r.buf, binary.BigEndian, value); err != nil {
@@ -82,11 +90,24 @@ func (r Reader) ReadPascalString() string {
 	}
 }
 
-func (r Reader) ReadRectangle() *Rectangle {
+func (r *Reader) ReadBytes(number interface{}) []byte {
+	n := getInteger(number)
+	value := make([]byte, n)
+	if err := binary.Read(r.buf, binary.BigEndian, value); err != nil {
+		panic(err)
+	}
+	r.Position += n
+	return value
+}
+
+func (r *Reader) ReadRectangle() *Rectangle {
+	r.Position += 16
 	return &Rectangle{r.ReadInt32(), r.ReadInt32(), r.ReadInt32(), r.ReadInt32()}
 }
 
-func (r Reader) Skip(n int32) {
+func (r *Reader) Skip(number interface{}) {
+	n := getInteger(number)
+	r.Position += n
 	if _, err := r.buf.Seek(int64(n), 1); err != nil {
 		panic(err)
 	}
--- section_resources.go
+++ section_resources.go
@@ -1,51 +1,83 @@
 package gopsd
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+)
 
 type ImageResource struct {
-	UID  int16
+	Id   int16
 	Name string
 	Data interface{}
 }
 
 func readResources(doc *Document) {
 	length := reader.ReadInt32()
-	if length == 0 {
-		return
-	}
 
 	doc.Resources = make(map[int16]*ImageResource)
-	pos := 0
+	startPos := 0
 
-	for pos < int(length) {
+	for startPos < int(length) {
 		ir := new(ImageResource)
+		pos := reader.Position
 
 		sign := reader.ReadString32()
 		if sign != "8BIM" {
 			panic(fmt.Sprintf("Wrong signature of resource [%d]!", len(doc.Resources)))
 		}
-		pos += 4
 
-		id := reader.ReadInt16()
-		pos += 2
-		ir.UID = id
-
-		name := reader.ReadPascalString()
-		pos += len(name) + 1
-		ir.Name = name
+		ir.Id = reader.ReadInt16()
+		ir.Name = reader.ReadPascalString()
 
 		size := reader.ReadInt32()
-		pos += 4
-
-		switch id {
-		default:
-			if size%2 != 0 {
-				size++
-			}
-			reader.Skip(size)
+		dataPos := reader.Position
+		switch ir.Id {
+		case 1033, 1036:
+			ir.Data = readResourceThumbnail(size)
 		}
-		pos += int(size)
+		if size%2 != 0 {
+			size++
+		}
+		reader.Skip(int(size) - (reader.Position - dataPos))
 
-		doc.Resources[id] = ir
+		startPos += reader.Position - pos
+		doc.Resources[ir.Id] = ir
 	}
+}
+
+type IRThumbnail struct {
+	Width  int32
+	Height int32
+	Image  image.Image
+}
+
+// http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577409_74450
+func readResourceThumbnail(size int32) *IRThumbnail {
+	thumb := new(IRThumbnail)
+
+	format := reader.ReadInt32()
+	thumb.Width = reader.ReadInt32()
+	thumb.Height = reader.ReadInt32()
+
+	reader.ReadInt32() // Widthbytes
+	reader.ReadInt32() // Total size
+	comprSize := reader.ReadInt32()
+
+	reader.ReadInt16() // Bits per pixel
+	reader.ReadInt16() // Number of planes
+
+	switch format {
+	case 0:
+	case 1:
+		img, err := jpeg.Decode(bytes.NewReader(reader.ReadBytes(comprSize)))
+		if err != nil {
+			panic(err)
+		}
+		thumb.Image = img
+	default:
+	}
+
+	return thumb
 }
